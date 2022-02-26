ee5a9f5063635af5741fad9e2871c298800f1591
Ashley Young <asyoung@redhat.com>
2022-02-26 10:00:00 -0500

    formatting intTempSensor output to CSV

--- intTempSensor.py
+++ intTempSensor.py
@@ -1,23 +1,17 @@
 import time
 import board
 import adafruit_htu31d
+import datetime
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 htu = adafruit_htu31d.HTU31D(i2c)
-#does not work out of box anymore
-#will delete this soon
-#print("Found HTU31D with serial number", hex(htu.serial_number))
-
-#won't be using heater on this project, make sure its off
-#htu.heater = True
-#print("Heater is on?", htu.heater)
 htu.heater = False
-#print("Heater is on?", htu.heater)
 
 temperature, relative_humidity = htu.measurements
 tempF = (temperature * 9/5) + 32
-print("Temperature: %0.1f F" % tempF)
-print("Humidity: %0.1f %%" % relative_humidity)
-print("")
-time.sleep(1)
+currentTime = datetime.datetime.now()
 
+print(str(currentTime) + ",", str(tempF) + ",", str(relative_humidity))
+
+#returns
+#2022-02-26 09:58:27.106682, 56.158480201419096, 44.576180666819255
