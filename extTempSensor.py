d91cf0e17cc82bc62e362ff9770413e68cbb29a0
Ashley Young <asyoung@redhat.com>
2022-02-26 10:08:07 -0500

    formatting extTempSensor output to CSV

--- extTempSensor.py
+++ extTempSensor.py
@@ -1,13 +1,14 @@
 import requests
 import json
+import datetime
 
 # API key API_KEY = "Your API Key"
-
 URL = 'https://api.openweathermap.org/data/2.5/weather?q=fayetteville,us&APPID=' + API_KEY + '&units=imperial'
 # HTTP request
 response = requests.get(URL)
 # checking the status code of the request
 if response.status_code == 200:
+   currentTime = datetime.datetime.now()
    # getting data in the json format
    data = response.json()
    # getting the main dict block
@@ -20,8 +21,9 @@ if response.status_code == 200:
    pressure = main['pressure']
    # weather report
    report = data['weather']
-   print(f"Temperature: {temperature}")
-   print(f"Humidity: {humidity}")
+   #print(f"Temperature: {temperature}")
+   #print(f"Humidity: {humidity}")
+   print(str(currentTime) + ",", str(temperature) + ",", str(humidity))
 else:
    # showing the error message
    print("Error in the HTTP request")
